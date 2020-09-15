a2edcaa12b21ea3188f0913e7c80d1a3ae4cb2c3
Llewellyn Falco <llewellyn.falco@gmail.com>
2020-09-14 17:27:46 -0700

    Update DigiFlower.java

--- LearnWithLlew/src/main/org/learnwithllew/courseware/section04mastery/DigiFlower.java
+++ LearnWithLlew/src/main/org/learnwithllew/courseware/section04mastery/DigiFlower.java
@@ -13,15 +13,11 @@ public class DigiFlower
     //    Make the background silver --#8
     //    Make the line the tortoise draws 3 pixels wide --#20
     //    createColorPalette (recipe below) --#9
-    //    Do the following 15 times --#19
-    //      drawOctogon (recipe below) --#10
-    //      Turn the tortoise 1/15th of 360 degrees to the right --#18
-    //    Repeat
     //    ------------- Recipe for createColorPalette --#9
-    //    Color 1 is red --#3
-    //    Color 2 is dark orange --#11
-    //    Color 3 is gold --#12
-    //    Color 4 is yellow --#13
+    //    Color 1 is red --#3.2
+    //    Color 2 is dark orange --#11.2
+    //    Color 3 is gold --#12.2
+    //    Color 4 is yellow --#13.2
     //    Add color 1 to the color wheel --#3.1
     //    Add color 2 to the color wheel --#11.1
     //    Add color 3 to the color wheel --#12.1
@@ -30,11 +26,17 @@ public class DigiFlower
     //    Add color 3 to the color wheel --#15
     //    Add color 2 to the color wheel --#16
     //    Add color 1 to the color wheel --#17
-    //    ------------- Recipe for drawOctogon --#10
-    //    Do the following 8 times --#6
-    //      Change the color of the line the tortoise draws to the next color on the color wheel --#4
-    //      Move the tortoise 50 pixels --#2
-    //      Turn the tortoise 1/8th of 360 degrees to the right --#5
-    //    Repeat  
+    //    ------------- End of createColorPalette recipe --#9
+    //    Do the following 15 times --#19
+    //      drawOctogon (recipe below) --#10
+    //      ------------- Recipe for drawOctogon --#10
+    //      Do the following 8 times --#6
+    //        Change the color of the line the tortoise draws to the next color on the color wheel --#4
+    //        Move the tortoise 50 pixels --#2
+    //        Turn the tortoise 1/8th of 360 degrees to the right --#5
+    //      Repeat  
+    //      ------------- End of drawOctogon recipe --#10
+    //      Turn the tortoise 1/15th of 360 degrees to the right --#18
+    //    Repeat
   }
 }
